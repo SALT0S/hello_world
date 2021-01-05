ccd66deef023a4d7d61a3887f6dd16110492c924
SALT0S <josesansalt@gmail.com>
2021-01-05 15:01:39 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new Center(
+      child: new Directionality(
+          textDirection: TextDirection.ltr, child: new Text("Hello Mundo"))));
+}
